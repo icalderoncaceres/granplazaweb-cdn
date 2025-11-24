a8955877b8b40f3cacfdd23503d78b31552cddd6
icalderoncaceres <icalderoncaceres@gmail.com>
2025-11-23 21:18:49 -0500

    Build v1.0.0 - 2025-11-24T02:18:49.148Z

--- assets/js/category-menu.js
+++ assets/js/category-menu.js
@@ -1,1 +1,43 @@
-document.addEventListener("DOMContentLoaded",function(){const e=document.getElementById("category-menu-bar"),n=document.getElementById("click-category-menu");e&&n&&(e.addEventListener("mouseenter",function(){n.classList.remove("d-none")}),n.addEventListener("mouseenter",function(){this.classList.remove("d-none")}),n.addEventListener("mouseleave",function(){this.classList.add("d-none")}),e.addEventListener("mouseleave",function(e){const t=e.relatedTarget;n.contains(t)||t===n||setTimeout(function(){n.matches(":hover")||n.classList.add("d-none")},50)}),document.querySelectorAll(".category-nav-element").forEach(function(e){const n=e.querySelector(".sub-cat-menu");n&&(e.addEventListener("mouseenter",function(){n.style.display="block"}),e.addEventListener("mouseleave",function(){n.style.display="none"}))}))});
+document.addEventListener("DOMContentLoaded", function () {
+    const e = document.getElementById("category-menu-bar"),
+        n = document.getElementById("click-category-menu");
+    if (e && n) {
+        e.addEventListener("mouseenter", function () {
+            n.classList.remove("d-none");
+        }),
+            n.addEventListener("mouseenter", function () {
+                this.classList.remove("d-none");
+            }),
+            n.addEventListener("mouseleave", function () {
+                this.classList.add("d-none");
+            }),
+            e.addEventListener("mouseleave", function (e) {
+                const t = e.relatedTarget;
+                n.contains(t) ||
+                    t === n ||
+                    setTimeout(function () {
+                        n.matches(":hover") || n.classList.add("d-none");
+                    }, 50);
+            });
+        let t = null;
+        document
+            .querySelectorAll(".category-nav-element")
+            .forEach(function (e) {
+                const n = e.querySelector(".sub-cat-menu");
+                n &&
+                    (e.addEventListener("mouseenter", function () {
+                        clearTimeout(t),
+                            (n.style.display = "block"),
+                            setTimeout(function () {
+                                n.classList.add("loaded");
+                            }, 10);
+                    }),
+                    e.addEventListener("mouseleave", function () {
+                        t = setTimeout(function () {
+                            (n.style.display = "none"),
+                                n.classList.remove("loaded");
+                        }, 400);
+                    }));
+            });
+    }
+});
